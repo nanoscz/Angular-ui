8f481fa0d621a62177a18f8b531b1d43751ee8f3
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-25 11:16:52 -0400

    :zap: Apply Boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/** Import module */
+import { NgxBootstrapModule } from './ngx-bootstrap.module'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ngx-bootstrap.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ButtonsModule } from 'ngx-bootstrap/buttons'
+
+const modules = [ButtonsModule]
+
+@NgModule({
+  declarations: [],
+  imports: modules,
+  exports: modules
+})
+export class NgxBootstrapModule { }
