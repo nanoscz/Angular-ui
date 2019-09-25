6a3c1b81783655eb8ea5339b757a684db76d21c6
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-25 00:59:22 -0400

    Using dynamic import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { PreloadingStrategyService } from './services/strategy/preloading-strategy.service';
 
 const routes: Routes = [
-  { path: 'first', loadChildren: './components/first/first.module#FirstModule', data: { preload: true } },
+  { 
+    path: 'first',
+    loadChildren: () => {
+      return import('./components/first/first.module').then(m => m.FirstModule)
+    },
+    data: { preload: true } 
+  },
   { path: 'second', loadChildren: './components/second/second.module#SecondModule' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    enableTracing: true,
+  enableTracing: true,
     preloadingStrategy: PreloadingStrategyService
   })],
   exports: [RouterModule]
